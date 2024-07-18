fa7bdec0ed69d53b045f8773261eb1a60fc5a92e
zackz <zackz@gmail.com>
2024-07-18 21:05:27 +1000

    debug

--- Python-flask/041-with-consul/user_service.py
+++ Python-flask/041-with-consul/user_service.py
@@ -32,5 +32,6 @@ def register_service():
         time.sleep(5)
 
 if __name__ == '__main__':
+    print("Registering user-service with Consul")
     register_service()
     app.run(host='0.0.0.0', port=5001)
