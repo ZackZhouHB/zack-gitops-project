137a79f613781958a42c20733383a3bb072257f0
zackz <zackz@gmail.com>
2024-10-04 16:50:18 +1000

    modify terraform var

---
+++ jenkins/terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-2"  # You can provide a default value or set it via the command line
+  description = "The AWS region where resources will be created."
+}
