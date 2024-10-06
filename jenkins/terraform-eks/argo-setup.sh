0069eb51bb4b971be689cdfee9538bec730f9821
zackz <zackz@gmail.com>
2024-10-06 23:50:10 +1100

    add argocd enable

---
+++ jenkins/terraform-eks/argo-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# install argocd
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sleep 60
+
+
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+argocd admin initial-password -n argocd
+
+argocd login <ARGOCD_SERVER>
