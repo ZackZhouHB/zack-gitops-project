99394e7c462da602bc4fa34c213ce3b55613b2bd
zack zhou <you@example.com>
2025-07-21 23:38:20 +1000

    upgrade to django 3.2

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -36,7 +36,7 @@ class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    paginate_by = 100
+    paginate_by = 20
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
