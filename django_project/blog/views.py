2167df75d3ec449b09fa149b654fd350bbaa99c9
zack zhou <zhbsoftboy1@gmail.com>
2025-07-22 00:58:29 +1000

    adjust page to 20 posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -29,7 +29,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 100
+    paginate_by = 20  # Reduced from 100 to 10 for better performance
 
 
 class UserPostListView(ListView):
